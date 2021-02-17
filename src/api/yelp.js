77e5099e82d8295d0d2e20033ca99ff6067509ee
Stu Bolderson <stu.bolderson@aol.com>
2021-02-17 13:13:03 +0100

    api request success, err msg renders to screen

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,3 +5,13 @@
 //             'wnMohCQeuGi5NG4URyT_T9JhJI80Wyysag_KtkTsZh7gCxBkfBiBeCW1OVdYi70ygSPA2m74jx933qiKoThU07sI-qrWmXsaMvaHXe7sf66TLHSPvBem0PBqr24qYHYx',
 //     },
 // ];
+
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization:
+            'Bearer wnMohCQeuGi5NG4URyT_T9JhJI80Wyysag_KtkTsZh7gCxBkfBiBeCW1OVdYi70ygSPA2m74jx933qiKoThU07sI-qrWmXsaMvaHXe7sf66TLHSPvBem0PBqr24qYHYx',
+    },
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SearchBar } from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 export const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: term,
+                    location: 'san jose',
+                },
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong !');
+        }
+    };
+
     return (
         <View style={styles.background}>
             <SearchBar
                 term={term}
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit={() => console.log('term submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={searchApi}
             />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 };
